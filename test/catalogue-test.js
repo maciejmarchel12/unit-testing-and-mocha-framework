c8fd5d50ccb7e3ff620b7a2bea6339ac22a83408
maciejmarchel12 <maciejmarchel14@gmail.com>
2023-09-28 16:16:26 +0100

    Completed checkReorder testing and refactored check reorder method

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -50,10 +50,12 @@ describe("Catalogue", () => {
         it("should include products just on their reorder level", function () {
           cat.addProduct(new Product("B125", "Product 6", 10, 10, 10.0));
           const result = cat.checkReorders();
-          // TO BE COMPLETED
+          expect(result.productIds).to.have.members(["B125"]);
         });
         it("should handle the empty catalogue case", function () {
-           // TO BE COMPLETED
+           cat = new Catalogue("Test Catalogue");
+           const result = cat.checkReorders();
+           expect(result.productIds).to.be.empty;
          });
       });
   });
